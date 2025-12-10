59fb24da6ca13afef29b04f787a573e9806e5808
ELLVO3 <maciek160805@gmail.com>
2025-12-10 11:04:26 +0100

    Add files via upload

--- cw9.py
+++ cw9.py
@@ -87,11 +87,42 @@ def alternateSum(b):
         suma += b[i]
     return suma
 
+###
+
+# Zadanie 8
+
+def list_appender(start, stop):
+    lista = []
+    for i in range(start, stop):
+        lista.append(i)
+    return lista
+
+def list_remover(lista):
+    for i in range(2, 100):
+        for j in lista[:]:
+            if j % i == 0 and j != i:
+                lista.remove(j)
+    return lista
+
+def list_remover2(lista):
+    for i in range(2, 100):
+        idx = 0
+        while idx < len(lista):
+            if lista[idx] % i == 0 and lista[idx] != i:
+                lista.pop(idx)
+                continue
+            idx += 1
+    return lista
+
+
 def main():
     a = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
     b = [1, 4, 9, 16, 9, 7, 4, 9, 11]
 
     print(minmax(a))
     print(alternateSum(b))
+
+    # print(list_remover(list_appender(2, 10001)))
+    print(list_remover2(list_appender(2, 10001)))
 
 main()
